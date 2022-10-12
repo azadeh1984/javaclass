9129d8544de057923b1f13cf297e24109e9d8adc
azadeh1984 <115475988+azadeh1984@users.noreply.github.com>
2022-10-12 23:55:45 +0200

    Add files via upload
    First Code

---
+++ Main.java
@@ -0,0 +1,13 @@
+package se.lexicon;
+
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name = "HELLO azadeh";
+        System.out.println(name);
+    }
+
+
+}
